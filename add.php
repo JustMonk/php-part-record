1f0be09c89e4c9f87d0ed4884484e708529ec85a
JustMonk <night.fire@mail.ru>
2019-12-07 23:39:39 +0500

    add-module UI improve

--- add.php
+++ add.php
@@ -18,6 +18,7 @@ include './include/auth_redirect.php';
    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
    <link href="./assets/font-awesome/css/all.css" rel="stylesheet">
    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700&display=swap&subset=cyrillic" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
@@ -31,7 +32,7 @@ include './include/auth_redirect.php';
          let globalState = <?php include './action/get_add_state.php' ?>
          //создаем состояние операции, которое будем накапливать
          globalState.incomeTable = new Map();
-         //.set(key, val)  .delete(key) .has(key)
+         //.set(key, val) .get(key)  .delete(key) .has(key)
          //.keys() .values() .entries()
       </script>
 
@@ -43,29 +44,37 @@ include './include/auth_redirect.php';
                   <h2 style="margin: 0">Приход</h2>
                   <hr>
                   <div id="test1" class="col s12" style="padding: 20px">
+
                      <div class="row" style="margin-bottom: 0;">
-
-                        <div class="input-field col s12">
-                           <input autocomplete="off" placeholder="Введите номер документа" id="first_name" type="text" class="">
-                           <label for="first_name">Номер документа</label>
+                        <div class="col s12 flex-col">
+                           <div class="input-field">
+                              <input autocomplete="off" placeholder="Введите номер документа" id="first_name" type="text" class="">
+                              <label for="first_name">Номер документа</label>
+                           </div>
+                           <i class="material-icons help-icon" data-tooltip="Введите номер операции или номер документа в 1С">help_outline</i>
                         </div>
+                     </div>
 
+                     <div class="row" style="margin-bottom: 0;">
                         <div class="col s6">
                            <div class="input-field">
                               <input id="operation-date" type="text" class="datepicker">
-                              <label for="operation-date" class="">Дата</label>
+                              <label for="operation-date" class="">Дата поступления</label>
                            </div>
                         </div>
 
-                        <div class="col s6">
-                           <div class="input-field col s12 autocomplete-field">
+                        <div class="col s6 flex-col">
+                           <div class="input-field autocomplete-field">
                               <input autocomplete="off" data-autocomplete-object="partners" id="partner-select" type="text" class="">
                               <label for="partner-select">Контрагент</label>
                            </div>
+                           <i class="material-icons help-icon" data-tooltip="Выберите контрагента из списка">help_outline</i>
                         </div>
+                     </div>
 
-                        <div class="col s12" style="margin: 40px 0px;">
-                           <table id="income-table" class="highlight">
+                     <div class="row" style="margin-bottom: 0;">
+                        <div class="input-field col s12" style="margin: 40px 0px;">
+                           <table id="income-table" class="product-table">
                               <thead>
                                  <tr>
                                     <th>№</th>
@@ -73,7 +82,7 @@ include './include/auth_redirect.php';
                                     <th>Количество</th>
                                     <th>Дата изготовления</th>
                                     <th>Срок годности</th>
-                                    <th>rm</th>
+                                    <th></th>
                                  </tr>
                               </thead>
 
@@ -83,76 +92,98 @@ include './include/auth_redirect.php';
                                  </tr>
                               </tbody>
                            </table>
-                           <a class="waves-effect waves-light btn blue-grey lighten-4 z-depth-0 modal-trigger" style="width: 100%; margin-top: 5px;" href="#add-modal">добавить позицию</a>
+                           <p id="empty-message" style="display: none;">Список продукции пуст, нажмите «добавить позицию».</p>
+                           <a class="waves-effect waves-light btn blue-grey lighten-4 z-depth-0" style="width: 100%; margin-top: 5px;" id="add-new-product">добавить позицию</a>
                         </div>
-
-                        <a id="create-record" class="waves-effect waves-light btn-large" style="width: 100%">Создать приход</a>
                      </div>
 
                   </div>
+                  <div class="divider" style="margin: 10px 0px;"></div>
+                  <a id="create-record" class="waves-effect waves-light btn-large" style="width: 100%">Создать приход</a>
                </div>
-
-
             </div>
 
 
          </div>
+
+
       </div>
+   </div>
    </div>
 
    <!-- Modal Structure (модалка для добавления, триггерится по нажатию кнопки "добавить позицию") -->
    <div id="add-modal" class="modal modal-fixed-footer">
       <div class="modal-content">
-         <h4>Добавление товара</h4>
-         <p>Выберите номенклатуру из списка ниже и заполните информацию о товаре</p>
+         <h4 id="modal-title" style="font-weight: bold;"></h4>
+         <p id="modal-desc"></p>
+         <h5>Основные поля</h5>
 
-         <div class="row">
-            <div class="col s12">
-            <h5>Основные поля</h5>
-               <div class="input-field col s12">
-                  <input autocomplete="off" data-autocomplete-object="goods" placeholder="Нажмите для выбора или поиска номенклатуры" id="goods-select" type="text" class="">
-                  <label for="goods-select">Номенклатура</label>
-               </div>
-
-               <div class="input-field col s12">
-                  <input autocomplete="off" placeholder="Введите количество товара" id="goods-count" type="text" class="">
-                  <label for="goods-count">Количество</label>
-               </div>
-
-               <div class="input-field col s6">
-                  <input id="goods-create-date" type="text" class="datepicker">
-                  <label for="goods-create-date" class="active">Дата изготовления</label>
-               </div>
-
-               <div class="input-field col s6">
-                  <input disabled id="valid-until" placeholder="Сначала введите дату изготовления" type="text">
-                  <label for="valid-until" class="active">Годен до</label>
-               </div>
-
-               <div id="extended-fields" style="display: none;">
-                  <h5>Дополнительные поля</h5>
-                  <div class="input-field col s4">
-                     <input id="ext-fat" placeholder="Введите процент жирности" type="text">
-                     <label for="ext-fat" class="active">Жирность</label>
+         <div id="main-fields">
+            <div class="row">
+               <div class="col s12 flex-col">
+                  <div class="input-field">
+                     <input autocomplete="off" data-autocomplete-object="goods" placeholder="Нажмите для выбора или поиска номенклатуры" id="goods-select" type="text" class="">
+                     <label for="goods-select">Номенклатура</label>
                   </div>
+                  <i class="material-icons help-icon" data-tooltip="Начните вводить название для поиска">help_outline</i>
+               </div>
+            </div>
 
-                  <div class="input-field col s4">
-                     <input id="ext-solidity" placeholder="Введите плотность" type="text">
-                     <label for="ext-solidity" class="active">Плотность</label>
-                  </div>
 
-                  <div class="input-field col s4">
-                     <input id="ext-acidity" placeholder="Введите кислотность" type="text">
-                     <label for="ext-acidity" class="active">Кислотность</label>
+            <div class="row">
+               <div class="col s12 flex-col">
+                  <div class="input-field">
+                     <input autocomplete="off" placeholder="Введите количество товара" id="goods-count" type="text" class="">
+                     <label for="goods-count">Количество</label>
                   </div>
+                  <i class="material-icons help-icon" data-tooltip="Количество товара">help_outline</i>
+               </div>
+            </div>
+
+            <div class="row">
+               <div class="col s6 flex-col">
+                  <div class="input-field">
+                     <input id="goods-create-date" type="text" class="datepicker">
+                     <label for="goods-create-date" class="active">Дата изготовления</label>
+                  </div>
+                  <i class="material-icons help-icon" data-tooltip="Введите дату изготовления">help_outline</i>
                </div>
 
+               <div class="col s6 flex-col">
+                  <div class="input-field">
+                     <input disabled id="valid-until" placeholder="Сначала выберите номенклатуру" type="text">
+                     <label for="valid-until" class="active">Срок годности</label>
+                  </div>
+                  <i class="material-icons help-icon" data-tooltip="Срок годности вычисляется автоматически">help_outline</i>
+               </div>
             </div>
          </div>
+
+         <div id="extended-fields" style="display: none;">
+            <h5>Дополнительные поля</h5>
+            <div class="row">
+               <div class="input-field col s4">
+                  <input id="ext-fat" placeholder="Введите процент жирности" type="text">
+                  <label for="ext-fat" class="active">Жирность</label>
+               </div>
+
+               <div class="input-field col s4">
+                  <input id="ext-solidity" placeholder="Введите плотность" type="text">
+                  <label for="ext-solidity" class="active">Плотность</label>
+               </div>
+
+               <div class="input-field col s4">
+                  <input id="ext-acidity" placeholder="Введите кислотность" type="text">
+                  <label for="ext-acidity" class="active">Кислотность</label>
+               </div>
+            </div>
+         </div>
+
+
+
       </div>
       <div class="modal-footer">
-         <a href="#!" id="add-product-button" class="modal-close waves-effect waves-green btn blue">Добавить</a>
-         <a href="#!" class="modal-close waves-effect waves-green btn grey">Отмена</a>
+         <!-- js controls -->
       </div>
    </div>
 
@@ -183,6 +214,7 @@ include './include/auth_redirect.php';
             onAutocomplete: function(elem) {
                if (globalState.goods[elem].extended_milk_fields) document.getElementById('extended-fields').style.display = 'block';
                else document.getElementById('extended-fields').style.display = 'none';
+               document.querySelector('#valid-until').value = globalState.goods[elem].valid_days;
             }
          });
       });
@@ -191,7 +223,7 @@ include './include/auth_redirect.php';
       document.addEventListener('DOMContentLoaded', function() {
          var elems = document.querySelector('#add-modal');
          var instances = M.Modal.init(elems, {
-            onOpenStart: clearAddModal
+            //onOpenStart: clearAddModal
          });
       });
 
@@ -206,6 +238,14 @@ include './include/auth_redirect.php';
          var elems = document.querySelector('#operation-date');
          var instances = M.Datepicker.init(elems, {});
       });
+
+      //вспывающие подсказки
+      M.Tooltip.init(document.querySelectorAll('.help-icon'), {
+         position: 'top'
+      });
+
+      //рендерим таблицу
+      incomeTableRender();
    </script>
 
 </body>
--- js/add_goods.js
+++ js/add_goods.js
@@ -17,50 +17,190 @@ function incomeTableRender() {
    let num = 1;
    let tableBody = document.getElementById('income-table').tBodies[0];
    tableBody.innerHTML = '';
-   for(let entry of globalState.incomeTable) {
+   for (let entry of globalState.incomeTable) {
       let tr = document.createElement('tr');
       tr.setAttribute('data-id', entry[0]);
+      tr.setAttribute('data-action', 'product-edit');
       tr.innerHTML = `<tr>
          <td>${num}</td>
          <td>${entry[1].name}</td>
          <td>${entry[1].count}</td>
          <td>${entry[1].createDate}</td>
          <td>${globalState.goods[entry[1].name].valid_days}</td>
-         <td>X</td>
+         <td><a class="delete-row-button"><i class="material-icons">delete_forever</i></a></td>
       </tr>`;
       tableBody.append(tr);
       num++;
    }
+
+   //сообщение о пустой таблице
+   if (!globalState.incomeTable.size) document.getElementById('empty-message').style.display = 'block';
+   else document.getElementById('empty-message').style.display = 'none';
 }
 
 document.addEventListener('click', (e) => {
+   let modal = M.Modal.getInstance(document.getElementById('add-modal'));
+   //обработчик добавления новой позиции
    if (e.target.id == 'add-product-button') {
+      if (!modalValidation()) return;
 
       let id = Math.floor(Math.random() * Math.floor(9999));
-      while(globalState.incomeTable.has(id)) {
+      while (globalState.incomeTable.has(id)) {
          id = Math.floor(Math.random() * Math.floor(9999));
       }
+
+      if (e.target.hasAttribute('data-id')) id = +e.target.getAttribute('data-id');
 
       globalState.incomeTable.set(id, {
          name: document.getElementById('goods-select').value,
          count: document.getElementById('goods-count').value,
-         createDate: document.getElementById('goods-create-date').value
+         createDate: document.getElementById('goods-create-date').value,
+         extFat: document.getElementById('ext-fat').value,
+         extSolidity: document.getElementById('ext-solidity').value,
+         extAcidity: document.getElementById('ext-acidity').value
       });
       console.log('добавлена строка в map');
       incomeTableRender();
+      modal.close();
+   }
+
+   //удаление ячейки из состояния и таблицы
+   if (e.target.closest('a') && e.target.closest('a').classList.contains('delete-row-button')) {
+      console.log('вошли в deleterow');
+      let row = e.target.closest('tr');
+      globalState.incomeTable.delete(+row.getAttribute('data-id'));
+      incomeTableRender();
+      return;
+   }
+
+   //модальное окно для добавления новой позиции
+   if (e.target.id == 'add-new-product') {
+      let modal = M.Modal.getInstance(document.getElementById('add-modal'));
+      clearAddModal();
+
+      modal.el.querySelector('#modal-title').innerText = 'Добавление товара';
+      modal.el.querySelector('#modal-desc').innerText = 'Выберите номенклатуру из списка ниже и заполните информацию о товаре';
+      let footer = modal.el.querySelector('.modal-footer');
+      footer.innerHTML = `
+      <a href="#!" id="add-product-button" class="waves-effect waves-green btn blue">Добавить</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn grey">Отмена</a>
+      `;
+
+      modal.open();
+   }
+
+   //модальное окно для редактирования существующей позиции
+   if (e.target.closest('tr') && e.target.closest('tr').getAttribute('data-action') == 'product-edit') {
+      let rowId = +e.target.closest('tr').getAttribute('data-id');
+      let modal = M.Modal.getInstance(document.getElementById('add-modal'));
+      clearAddModal();
+
+      //общие поля
+      modal.el.querySelector('#goods-select').value = globalState.incomeTable.get(rowId).name;
+      modal.el.querySelector('#goods-count').value = globalState.incomeTable.get(rowId).count;
+      modal.el.querySelector('#goods-create-date').value = globalState.incomeTable.get(rowId).createDate;
+      modal.el.querySelector('#valid-until').value = globalState.goods[globalState.incomeTable.get(rowId).name].valid_days;
+      //дополнительные поля для сырого молока
+      modal.el.querySelector('#ext-fat').value = globalState.incomeTable.get(rowId).extFat;
+      modal.el.querySelector('#ext-solidity').value = globalState.incomeTable.get(rowId).extSolidity;
+      modal.el.querySelector('#ext-acidity').value = globalState.incomeTable.get(rowId).extAcidity;
+      //показать доп. поля, если редактируем сырое молоко
+      if (globalState.goods[globalState.incomeTable.get(rowId).name].extended_milk_fields) modal.el.querySelector('#extended-fields').style.display = 'block';
+
+      modal.el.querySelector('#modal-title').innerText = 'Редактирование товара';
+      modal.el.querySelector('#modal-desc').innerText = 'Внесите необходимые изменения и нажмите сохранить';
+      let footer = modal.el.querySelector('.modal-footer');
+      footer.innerHTML = `
+      <a href="#!" id="add-product-button" data-id="${rowId}" class="waves-effect waves-green btn blue">Сохранить</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn grey">Отмена</a>
+      `;
+
+      modal.open();
+      M.updateTextFields();
    }
 
    //заглушка под добавление
    if (e.target.id == 'create-record') {
-      M.toast({html: 'Приход создан! (но записи в БД пока нет, тестовая версия)'})
+      if (!formValidation()) return;
+      M.toast({ html: 'Приход создан! (но записи в БД пока нет, тестовая версия)' });
    }
 })
+
+function modalValidation() {
+   let isValid = true;
+   let modal = M.Modal.getInstance(document.getElementById('add-modal'));
+
+   if (modal.el.querySelector('#goods-select').value.length < 1) {
+      isValid = false;
+      modal.el.querySelector('#goods-select').className = 'validate invalid';
+   }
+
+   if (modal.el.querySelector('#goods-count').value.length < 1) {
+      isValid = false;
+      modal.el.querySelector('#goods-count').className = 'validate invalid';
+   }
+
+   if (modal.el.querySelector('#goods-create-date').value.length < 1) {
+      isValid = false;
+      modal.el.querySelector('#goods-create-date').className = 'validate invalid';
+   }
+
+   //проверка на доп.поля (если требуется)
+   if (globalState.goods[modal.el.querySelector('#goods-select').value].extended_milk_fields) {
+      if (modal.el.querySelector('#ext-fat').value.length < 1) {
+         isValid = false;
+         modal.el.querySelector('#ext-fat').className = 'validate invalid';
+      }
+
+      if (modal.el.querySelector('#ext-solidity').value.length < 1) {
+         isValid = false;
+         modal.el.querySelector('#ext-solidity').className = 'validate invalid';
+      }
+
+      if (modal.el.querySelector('#ext-acidity').value.length < 1) {
+         isValid = false;
+         modal.el.querySelector('#ext-acidity').className = 'validate invalid';
+      }
+   }
+
+   return isValid;
+}
+
+function formValidation() {
+   let isValid = true;
+
+   if (document.getElementById('first_name').value.length < 1) {
+      isValid = false;
+      document.getElementById('first_name').className = 'validate invalid';
+   }
+
+   if (document.getElementById('operation-date').value.length < 1) {
+      isValid = false;
+      document.getElementById('operation-date').className = 'validate invalid';
+   }
+
+   if (document.getElementById('partner-select').value.length < 1) {
+      isValid = false;
+      document.getElementById('partner-select').className = 'validate invalid';
+   }
+
+   //не пустая таблица (состояние)
+   if (!globalState.incomeTable.size) {
+      isValid = false;
+      M.toast({ html: 'Необходимо добавить хотя бы один товар' });
+   }
+
+   return isValid;
+}
 
 //ощищает модальную форму добавления и скрывает доп.поля
 function clearAddModal() {
    let modalWrapper = document.getElementById('add-modal');
    document.getElementById('extended-fields').style.display = 'none';
-   modalWrapper.querySelectorAll('input').forEach(val => { val.value = '' });
+   modalWrapper.querySelectorAll('input').forEach(val => {
+      val.value = '';
+      val.className = '';
+   });
    M.updateTextFields();
 }
 
@@ -70,5 +210,6 @@ document.addEventListener('focusout', (e) => {
    if (!globalState[e.target.getAttribute('data-autocomplete-object')].hasOwnProperty([e.target.value])) {
       e.target.value = '';
       M.updateTextFields();
+      clearAddModal();
    }
 });
